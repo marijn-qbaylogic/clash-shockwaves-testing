0dda29cd2ecde408a32daca131fdbf9ecf5ad0a3
Marijn <marijn@qbaylogic.com>
2026-02-16 16:27:02 +0100

    Added github workflow for automatic compilation of the surfer plugin

--- surfer_shockwaves/compile.sh
+++ surfer_shockwaves/compile.sh
@@ -1,13 +1,19 @@
 cargo build --target wasm32-unknown-unknown
 
+WASM = target/wasm32-unknown-unknown/debug/surfer_shockwaves.wasm
+
 # optionally:
 if [ "$1" = "linux" ]; then
-  cp target/wasm32-unknown-unknown/debug/surfer_shockwaves.wasm ~/.local/share/surfer/translators/
+  cp $FILE ~/.local/share/surfer/translators/
   echo "copied to Linux folder"
 fi
 if [ "$1" = "windows" ]; then
-  cp target/wasm32-unknown-unknown/debug/surfer_shockwaves.wasm ~/AppData/Roaming/surfer-project/surfer/data/translators
+  cp $FILE ~/AppData/Roaming/surfer-project/surfer/data/translators
   echo "copied to Windows folder"
+fi
+if [ "$1" = "here" ]; then
+  cp $FILE .
+  echo "copied to this folder"
 fi
 
 echo "[DONE]"
