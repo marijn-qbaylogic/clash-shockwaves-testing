08be3e2232ee4d2f9609e132c7d5bf3117ffc69b
Marijn <marijn@qbaylogic.com>
2026-02-17 15:45:17 +0100

    Pipeline fixes

--- surfer_shockwaves/compile.sh
+++ surfer_shockwaves/compile.sh
@@ -1,20 +1,23 @@
+#!/bin/bash
+set -e
+
 cargo build --target wasm32-unknown-unknown
 
-WASM = target/wasm32-unknown-unknown/debug/surfer_shockwaves.wasm
+WASM="target/wasm32-unknown-unknown/debug/surfer_shockwaves.wasm"
 
 # optionally:
 if [ "$1" = "linux" ]; then
-  cp $FILE ~/.local/share/surfer/translators/
+  cp $WASM ~/.local/share/surfer/translators/
   echo "copied to Linux folder"
 fi
 if [ "$1" = "windows" ]; then
-  cp $FILE ~/AppData/Roaming/surfer-project/surfer/data/translators
+  cp $WASM ~/AppData/Roaming/surfer-project/surfer/data/translators
   echo "copied to Windows folder"
 fi
 if [ "$1" = "here" ]; then
-  cp $FILE .
+  cp $WASM .
   echo "copied to this folder"
 fi
 
 echo "[DONE]"
-sleep 5
+# sleep 5
